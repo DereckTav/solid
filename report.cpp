0b42f114bf3876b3012a52be7cbf467bd7611ace
Dereck Taverne <derecktaverne@gmail.com>
2025-10-22 16:33:06 -0400

    Refactor Report

--- report.cpp
+++ report.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
 #include <sstream>
+#include <string>
 
-class Report {
+class DataProcessor {
 public:
-    std::string title;
-    std::string data;
-
-    Report(const std::string& t, const std::string& d) : title(t), data(d) {}
-
-    int processData() {
+    int process(const std::string& data) const {
         std::istringstream iss(data);
         int number, sum = 0;
         while (iss >> number) {
@@ -16,15 +12,36 @@ public:
         }
         return sum;
     }
+};
 
-    std::string format() {
-        return "<h1>" + title + "</h1>\n<p>" + title + ": " + std::to_string(processData()) + "</p>";
+class Formatter {
+public:
+    std::string format(const std::string& title, int result) const {
+        return "<h1>" + title + "</h1>\n<p>" + title + ": " + std::to_string(result) + "</p>";
+    }
+};
+
+class Report {
+public:
+    std::string title;
+    std::string data;
+    DataProcessor processor;
+    Formatter formatter;
+
+    Report(const std::string& t, const std::string& d): title(t), data(d) {
+        processor = DataProcessor(); 
+        formatter = Formatter(); 
     }
 
+    std::string generateReport() const {
+        int result = processor.process(data);
+        return formatter.format(title, result);
+    }
 };
+
 
 // Included for demonstration
 int main() {
     Report report("Total sales", "51 42 39 60");
-    std::cout << report.format();
+    std::cout << report.generateReport();
 }
